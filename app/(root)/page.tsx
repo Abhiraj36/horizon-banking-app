2db8e189d33f18643e210e1788a0ebbfc6622709
Abhiraj Aryan <emailabhiraj061@gmail.com>
2026-04-25 17:30:14 +0530

    Home Page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: "Abhiraj" };
+
   return (
-    <div>page</div>
+    <section className='home'>
+      <div className = "home-content">
+        <header className = "home-header">
+         <HeaderBox 
+         type = "greeting"
+         title = "Welcome"
+         user = {loggedIn?.firstName || 'Guest'}
+         subtext = "Access and manage your account and transactions efficiently."
+         />
+         <TotalBalanceBox 
+         accounts = { [] }
+         totalBanks={1}
+         totalCurrentBalance={1345.89}
+         />
+        </header>
+      </div>
+    </section>
   )
 }
 
---
+++ components/ui/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <span className="w-full">
+      <CountUp
+        duration = {2}
+        decimal=","
+        prefix="₹"
+        end={amount}
+      />
+    </span>
+  );
+};
+
+export default AnimatedCounter;
